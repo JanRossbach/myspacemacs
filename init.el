5ec645bde27dfafe99ab6255587771e4e26d72e1
JanRossbach <jan.rossbach@hhu.de>
2022-01-20 22:52:00 +0100

    Add some cool doom themes to the Themes list

--- init.el
+++ init.el
@@ -251,6 +251,10 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(doom-one
+                         doom-spacegrey
+                         doom-palenight
+                         doom-vibrant
+                         doom-snazzy
                          doom-city-lights
                          spacemacs-dark
                          spacemacs-light)
